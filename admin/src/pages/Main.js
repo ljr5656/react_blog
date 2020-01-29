10e6fac1769946580e8b03749d9e80eab638fac2
ljr5656 <1368298676@qq.com>
2020-01-29 22:09:56 +0800

    完成后台管理系统路由配置及布局

--- admin/src/pages/Main.js
+++ admin/src/pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Login from './Login'
+import AdminIndex from './AdminIndex'
 
 function Main() {
   return (
     <Router>
       <Route path="/login/" exaxt component={Login}></Route>
+      <Route path="/index/" exaxt component={AdminIndex}></Route>
     </Router>
   )
 }
