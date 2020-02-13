1238f5c58e46309779b0a6d111e8ae2b94baceef
ljr5656 <1368298676@qq.com>
2020-02-13 19:41:26 +0800

    添加打包文件

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -33,7 +33,7 @@ module.exports = appInfo => {
       // username
       user: 'root',
       // password
-      password: 'root',
+      password: '123456',
       // database
       database: 'react_blog',
     },
