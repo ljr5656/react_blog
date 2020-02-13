c63d970b86ff94edc6444e7384cb65727db122fb
ljr5656 <1368298676@qq.com>
2020-02-13 19:39:46 +0800

    添加打包文件

---
+++ admin/build/precache-manifest.d0f151bf7f50f17b702612aa9124f158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3ea767aaf024877d0d7446d1e9bb31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4f10661188e9f97d2e",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "3aabdf50524a74c3d52b",
+    "url": "/static/css/main.81577fc9.chunk.css"
+  },
+  {
+    "revision": "6d4f10661188e9f97d2e",
+    "url": "/static/js/2.48eea983.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.48eea983.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aabdf50524a74c3d52b",
+    "url": "/static/js/main.aa1dba74.chunk.js"
+  },
+  {
+    "revision": "ead00dad6c75563c7a7e",
+    "url": "/static/js/runtime-main.79802dc9.js"
+  }
+]);
